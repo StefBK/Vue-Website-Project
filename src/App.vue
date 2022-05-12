bbcdd3cfcb93bd84b6a54c750452f1f782b6e07a
Stef BK <stefsbk@gmail.com>
2022-05-12 12:37:53 +0200

    Adding Footer & Home

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
 import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
@@ -22,4 +23,5 @@ import Navbar from '@/components/Navbar.vue';
   </header>
 
   <RouterView />
+  <Footer />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+// import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
+  <!-- <main>
     <TheWelcome />
-  </main>
+  </main> -->
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h1>Welcome to my Vue Site!</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <img src="@/assets/Husky.jpg" class="img-fluid" alt="Husky">
+      </div>
+      <div class="col-12  col-md-6">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus necessitatibus voluptatum quaerat harum nostrum aperiam sunt similique alias eius, non repellat asperiores, a consequuntur! Sapiente a debitis molestias esse fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus necessitatibus voluptatum quaerat harum nostrum aperiam sunt similique alias eius, non repellat asperiores, a consequuntur! Sapiente a debitis molestias esse fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus necessitatibus voluptatum quaerat harum nostrum aperiam sunt similique alias eius, non repellat asperiores, a consequuntur! Sapiente a debitis molestias esse fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus necessitatibus voluptatum quaerat harum nostrum aperiam sunt similique alias eius, non repellat asperiores, a consequuntur! Sapiente a debitis molestias esse fugit. </p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+  img{
+    width: 100%;
+  }
+</style>
